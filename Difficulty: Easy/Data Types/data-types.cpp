4723ffc823775effc115b95d2d6c20a1cfbb3fb7
Viraj Upadhyay <virajupadhyay16@gmail.com>
2025-07-19 23:14:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Data Types/data-types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+int main() {
+    int a;
+    float b;
+    double c;
+    long long d;
+    // variables declaration checking
+    if ((typeid(a) == typeid(int)) and (typeid(b) == typeid(float)) and
+        (typeid(c) == typeid(double)) and (typeid(d) == typeid(long long)))
+        cout << "verified\n";
+    return 0;
+}
